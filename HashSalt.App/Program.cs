71040147afea393c3b68c5b04cfce7bbcb34f334
Aghost <youri_van_sorgen2@example.com>
2021-10-18 23:12:16 +0200

    initial

---
+++ HashSalt.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using static System.Console;
+using static HashSalt.Lib.HashSalter;
+
+namespace HashSalt.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salt = GetSalt(24);
+            string tmp = GetHash("henk", salt);
+
+            WriteLine(tmp);
+            WriteLine( ConfirmHash("henk", tmp, salt));
+        }
+    }
+}
---
+++ HashSalt.Lib/HashSalter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace HashSalt.Lib
+{
+    public static class HashSalter
+    {
+        public static string GetSalt(int size) {
+            RNGCryptoServiceProvider rng = new();
+            byte[] buffer = new byte[size];
+
+            // Fill buffer with random sequence of values
+            rng.GetBytes(buffer);
+
+            return Convert.ToBase64String(buffer);
+        }
+
+        public static string GetHash(string input, string salt) {
+            byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
+            SHA256Managed shaString = new();
+
+            byte[] hash = shaString.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool ConfirmHash(string text, string hashed, string salt) {
+            string newHashPin = GetHash(text, salt);
+
+            return newHashPin.Equals(hashed);
+        }
+    }
+}
